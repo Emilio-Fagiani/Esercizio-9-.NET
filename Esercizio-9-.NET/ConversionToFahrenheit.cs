093dbd58fc646c5c354305ec439c0a9d98c97e35
emifa <emifa@LAPTOP-1TM449QB>
2023-06-24 12:37:37 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-9-.NET/ConversionToFahrenheit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_9_.NET
+{
+    public class ConversionToFahrenheit
+    {
+       public  double ConversionFahrenheit(double inp)
+        {
+            double converter = inp * 1.8 + 32;
+            return converter;
+
+        }
+
+    }
+}
---
+++ Esercizio-9-.NET/Program.cs
@@ -0,0 +1,10 @@
+﻿using Esercizio_9_.NET;
+
+Console.WriteLine("Please enter the Celcius");
+string input = Console.ReadLine(); 
+double inp = int.Parse(input);  
+
+ConversionToFahrenheit ctf = new ConversionToFahrenheit();
+var convert = ctf.ConversionFahrenheit(inp);
+Console.WriteLine(convert);
+
